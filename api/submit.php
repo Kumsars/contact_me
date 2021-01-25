bc128b1cfb9fe794d68573bb3ef11988132efe8c
Kumsars <jankaka10@gmail.com>
2021-01-25 17:16:59 +0200

    Izmaiņas

--- api/submit.php
+++ api/submit.php
@@ -1,4 +1,12 @@
 <?php
-require_once 'db_config.php';
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+    require_once 'db_config.php';
+
+    $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+    $data = json_decode(file_get_contents("index.html"));
 ?>
